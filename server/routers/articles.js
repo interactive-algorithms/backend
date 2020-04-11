bca880f0b10884f495a2790ff7a53b31b2ea73d2
williamBMDK <williambillemeyling@gmail.com>
2020-04-11 22:05:46 +0200

    GET single article

--- server/routers/articles.js
+++ server/routers/articles.js
@@ -33,6 +33,66 @@ router.get("/", (req, res) => {
         res.send({articles})
     })
 })
-//articles.id as articleID, articles.title as articleTitle
+
+router.get("/:id", (req, res) => {
+    const articleID = req.params.id;
+    pool
+    .query(
+        `SELECT
+            articles.id as articleID,
+            articles.title as articleTitle,
+            sections.title as sectionTitle,
+            sections.id as sectionID,
+            type,
+            content,
+            URL,
+            alt,
+            description
+        FROM articles
+        RIGHT JOIN
+            sections ON sections.articleID = articles.id
+        RIGHT JOIN
+            generic_items ON sections.id = generic_items.sectionID
+        LEFT JOIN
+            images ON generic_items.id = images.genericItemID 
+        WHERE sections.articleID = ? 
+        ORDER BY sections.position, generic_items.position
+        `,
+        [articleID]
+    ).then(([items]) => {
+        const sectionIndices = {}
+        const article = {
+            sections : []
+        }
+        for(const item of items){
+            if(!article.title){
+                article.title = item.articleTitle;
+            }
+            if(sectionIndices[item.sectionID] == undefined){
+                sectionIndices[item.sectionID] = article.sections.length;
+                article.sections.push({
+                    title : item.sectionTitle,
+                    id : item.sectionID,
+                    content : []
+                })
+            }
+            const obj = item.type == 'img' ?
+                {
+                    url : item.URL,
+                    alt : item.alt,
+                    description : item.description,
+                    type : "img"
+                } : {
+                    type : item.type,
+                    content : item.content
+                }
+            ;
+            article.sections[sectionIndices[item.sectionID]].content.push(obj);
+        }
+        res.send({
+            article
+        })
+    })
+})
 
 module.exports = router;
