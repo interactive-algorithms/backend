4f41fe9b87147ef4036ea849056e8348ed6592b2
williamBMDK <williambillemeyling@gmail.com>
2020-04-11 16:07:27 +0200

    fix oken expiration

--- server/routers/middleware/auth.js
+++ server/routers/middleware/auth.js
@@ -5,13 +5,16 @@ const authorize = (req, res, next) => {
     if(!token) {
         res.sendStatus(401);
     } else {
-        jwt.verify(token, process.env.TOKEN_SECRET, (err, data) => {
-            if (err) throw err;
+        jwt.verify(token, process.env.TOKEN_SECRET, (err, data) => {            
+            if (err.name == "TokenExpiredError"){
+                res.status(401).send("token expired");
+            }else if(err){
+                throw err;
+            }
+
             if (data) {
                 req.username = data.username;
                 next();
-            } else {
-                res.sendStatus(401);
             }
         });
     }
@@ -20,7 +23,9 @@ const authorize = (req, res, next) => {
 const sendToken = (req, res, next) => {
     jwt.sign({
         username : req.body.username 
-    }, process.env.TOKEN_SECRET, (err, token) => {
+    }, process.env.TOKEN_SECRET, {
+        expiresIn: 60
+    }, (err, token) => {
         if(err){
             res.sendStatus(500);
         }else{
