366ecada44ad01ce0635f0f76962cd21059e7e70
williamBMDK <williambillemeyling@gmail.com>
2020-04-11 20:53:47 +0200

    GET all articles

--- server/routers/articles.js
+++ server/routers/articles.js
@@ -3,7 +3,36 @@ const express = require("express")
 const router = express.Router();
 
 router.get("/", (req, res) => {
-    res.send("articles")
+    pool
+    .query(
+        `SELECT 
+            articles.id as articleID,
+            articles.title as articleTitle,
+            sections.title as sectionTitle,
+            sections.id as sectionID
+        FROM articles
+        RIGHT JOIN
+            sections ON articles.id = sections.articleID
+        ORDER BY
+            position`
+    ).then(([sections]) => {
+        const articles = {};
+        for(const section of sections){
+            if(!articles[section.articleID]){
+                articles[section.articleID] = {
+                    sections : [],
+                    title : section.articleTitle,
+                    id : section.articleID
+                }
+            }
+            articles[section.articleID].sections.push({
+                id : section.sectionID,
+                sectionTitle : section.sectionTitle
+            })
+        }
+        res.send({articles})
+    })
 })
+//articles.id as articleID, articles.title as articleTitle
 
 module.exports = router;
